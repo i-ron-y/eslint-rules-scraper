f6228589153bd5e5c367b4483bd3443675827d72
Ron Y <31330116+i-ron-y@users.noreply.github.com>
2017-10-11 23:22:23 -0700

    Write properly-formatted output to .eslintrc.json; update README

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # ESLint Rules Scraper
 
-Extracts rules from ESLint's 'List of available rules' page.
+Extracts rules from ESLint's [List of available rules](https://eslint.org/docs/rules/) page.
 
 
 ## Usage
 
-1. Install Python.
+Install Python.
 
-2. Install the required packages:
+Install the required packages:
 
 ````
 pip install urllib3
 pip install BeautifulSoup4
 ````
 
-3. Go to the directory where `eslint-rules-scraper.py` is located.
+Go to the directory where `eslint-rules-scraper.py` is located.
+
+Run:
 
 ````
 python eslint-rules-scraper.py
 ````
+
+A .eslintrc.json file (containing ONLY the rules) will be outputted in the same directory where eslint-rules-scraper.py is found.
+
+**WARNING**: If you have a .eslintrc.json file in this directory, it WILL be overwritten.
--- eslint-rules-scraper.py
+++ eslint-rules-scraper.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://eslint.org/docs/rules/")
+# Here is where the data scraping happens.
+
+page = requests.get('https://eslint.org/docs/rules/')
 soup = BeautifulSoup(page.content, 'html.parser')
 
 
@@ -23,7 +25,7 @@ ruleGroups = []
 for i in range(len(typeIds)):
 	header = soup.find(id=typeIds[i])
 
-	table = header.find_next_sibling("table")
+	table = header.find_next_sibling('table')
 
 	# e.g. [RuleName1, RuleDef1, RuleName2, RuleDef2, ...]
 	rulesAll = []
@@ -41,3 +43,45 @@ for i in range(len(typeIds)):
 
 	ruleGroups.append(ruleGroup)
 
+
+# Here is where the .eslintrc.json file (containing ONLY the rules) is outputted, in the same directory where eslint-rules-scraper.py is found.
+# WARNING: If you have a .eslintrc.json file in this directory, it WILL be overwritten.
+
+linebreak = '\n'*2
+indent = ' '*4
+commentHeader = '/'*8
+commentDefn = '// '
+columnDefn = 48
+
+usageStringLineStart = indent*2 + '//'
+
+usageString = (usageStringLineStart + ' Usage:\n' +
+			   usageStringLineStart + indent + '"off" or 0 - turn the rule off\n' +
+			   usageStringLineStart + indent + '"warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)\n' +
+			   usageStringLineStart + indent + '"error" or 2 - turn the rule on as an error (exit code is 1 when triggered)\n' +
+			   usageStringLineStart + '\n' +
+			   usageStringLineStart + indent + 'If a rule has additional options, you can specify them using array literal syntax, such as:\n' +
+			   usageStringLineStart + indent*2 + '"quotes": [2, "double"]\n')
+
+outputString = '{' + linebreak + indent + '"rules":' + linebreak + usageString + linebreak
+
+for rg in range(len(ruleGroups)):
+	headerString = indent*2 + commentHeader + ' ' + ruleGroups[rg][0] + ' ' + commentHeader
+
+	groupRulesString = ''
+
+	for rn in range(len(ruleGroups[rg][1])):
+		ruleNameString = indent*2 + '"' + ruleGroups[rg][1][rn][0] + '": 0,'
+		ruleDefnString = commentDefn + ruleGroups[rg][1][rn][1]
+		ruleString = ruleNameString + ' '*(columnDefn-len(ruleNameString)) + ruleDefnString
+		groupRulesString += ruleString + '\n'
+
+	groupString = headerString + linebreak + groupRulesString
+
+	outputString += groupString + linebreak
+
+outputString += '}'
+
+f = open('.eslintrc.json', 'w')
+f.write(outputString)
+f.close()
